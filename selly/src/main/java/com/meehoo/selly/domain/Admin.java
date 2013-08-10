d8a52981b9a9509057f2b411d6fea4f9d4b5de45
wangboa123 <wangboa123@163.com>
2013-08-10 22:46:37 +0800

    配置好了。只是在生成实体对象的时候不能使用uuid2,只能使用uuid，这个还是需要改的

--- selly/src/main/java/com/meehoo/selly/domain/Admin.java
+++ selly/src/main/java/com/meehoo/selly/domain/Admin.java
@@ -8,30 +8,52 @@
 package com.meehoo.selly.domain;
 import java.util.*;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
-
+import org.hibernate.id.factory.*;
 /** @pdOid 8f1e6fc8-de03-4a49-b8b0-3c5b35839389 */
 @Entity
 @Table(name="tadmin")
 public class Admin {
  
+	 /** @pdOid 26888565-c76f-477c-8b76-3c73b4b72a9b */
+	   @Id
+	   @GeneratedValue(generator="uuid")
+	   @GenericGenerator(name="uuid",strategy="uuid")
+	   public String id;
+	   
+	   /** @pdOid f735ae99-e671-4f07-a394-d3106361c887 */
+		@Column(nullable=false)
+	   public String adminId;
+	   /** @pdOid 53e2687e-bfbf-4a1b-bef7-dea84f9aff32 */
+	   @Column(nullable=false)
+		public String adminPwd;
+	     
 	
 	
-	/** @pdOid f735ae99-e671-4f07-a394-d3106361c887 */
-	@Column(nullable=false)
-   public java.lang.String adminId;
-   /** @pdOid 53e2687e-bfbf-4a1b-bef7-dea84f9aff32 */
-   @Column(nullable=false)
-	public java.lang.String adminPwd;
-   /** @pdOid 26888565-c76f-477c-8b76-3c73b4b72a9b */
-   @Id
-   @GeneratedValue(generator="uuid2")
-   @GenericGenerator(name="uuid2",strategy="uuid2")
-   public java.lang.String id;
+	public String getAdminPwd() {
+		return adminPwd;
+	}
+	public void setAdminPwd(String adminPwd) {
+		this.adminPwd = adminPwd;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+public String getAdminId() {
+	return adminId;
+}
+public void setAdminId(String adminId) {
+	this.adminId = adminId;
+}
 
 }
